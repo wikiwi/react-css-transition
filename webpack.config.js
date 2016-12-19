b90588cfaa3c59f9c37b570660470e338f948ccf
Chi Vinh Le <vinh@wikiwi.io>
2016-12-19 20:58:35 +0700

    Update dependencies & fix build issues

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@
 const fs = require("fs");
 const webpack = require("webpack");
 
+const useMinification = process.env.BUILD_MINIFIED === "true";
 const packageName = JSON.parse(fs.readFileSync("package.json", "utf8")).name;
 const libraryName = packageName.replace(/(^|-)(.)/g, (match, _, c) => (c ? c.toUpperCase() : ""));
 
@@ -46,4 +47,8 @@ const minified = Object.assign({}, config, {
   ]),
 });
 
-module.exports = [config, minified];
+// @TODO let webpack run configurations in parallel after solving:
+// https://github.com/s-panferov/awesome-typescript-loader/issues/323
+module.exports = useMinification
+  ? minified
+  : config;
