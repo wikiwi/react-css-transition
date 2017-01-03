34fdb24496b8dfca223b2fc01bf36e4a4cccb7eb
Chi Vinh Le <vinh@wikiwi.io>
2017-01-03 18:58:21 +0700

    Update Copyright header

--- src/transitionobserver.spec.tsx
+++ src/transitionobserver.spec.tsx
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright (C) 2016 Chi Vinh Le and contributors.
+ * Copyright (C) 2016-present Chi Vinh Le and contributors.
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
