ba2ccb6e3846c6f675375eba5fbe67a7541aa964
Chi Vinh Le <vinh@wikiwi.io>
2017-01-10 20:54:13 +0700

    Set noEmit

---
+++ src/global.d.ts
@@ -0,0 +1,9 @@
+/**
+ * @license
+ * Copyright (C) 2017-present Chi Vinh Le and contributors.
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ */
+
+declare module "fbjs/lib/shallowEqual";
