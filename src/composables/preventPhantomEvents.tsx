52188c7c2e81ab70d1af24912143d3d7719273f8
Chi Vinh Le <vinh@wikiwi.io>
2017-01-30 09:40:01 +0700

    Remove unneeded combine

--- src/composables/preventPhantomEvents.tsx
+++ src/composables/preventPhantomEvents.tsx
@@ -1,5 +1,5 @@
 import { EventHandler, TransitionEvent } from "react";
-import { combine, withHandlers, onWillReceiveProps, onDidUpdate, isolate, integrate } from "reassemble";
+import { withHandlers, onWillReceiveProps, onDidUpdate, isolate, integrate } from "reassemble";
 
 import { CSSTransitionProps } from "../csstransition";
 
@@ -11,55 +11,53 @@ type PropsOut = {
 
 type PropsUnion = CSSTransitionProps & PropsOut;
 
-export const preventPhantomEvents = combine(
-  isolate(
-    withHandlers<PropsUnion, PropsOut>(
-      () => {
-        let lastTriggerTime: any;
-        let lastTriggerTimePerformance: any;
-        let timeUpdateRequested = false;
-        return {
-          requestTimeUpdate: () => () => {
-            timeUpdateRequested = true;
-          },
-          handleTimeUpdateRequest: () => () => {
-            if (timeUpdateRequested) {
-              lastTriggerTime = Date.now();
-              if (typeof performance !== "undefined" &&
-                typeof performance.now !== "undefined") {
-                lastTriggerTimePerformance = performance.now();
-              }
-              timeUpdateRequested = false;
+export const preventPhantomEvents = isolate(
+  withHandlers<PropsUnion, PropsOut>(
+    () => {
+      let lastTriggerTime: any;
+      let lastTriggerTimePerformance: any;
+      let timeUpdateRequested = false;
+      return {
+        requestTimeUpdate: () => () => {
+          timeUpdateRequested = true;
+        },
+        handleTimeUpdateRequest: () => () => {
+          if (timeUpdateRequested) {
+            lastTriggerTime = Date.now();
+            if (typeof performance !== "undefined" &&
+              typeof performance.now !== "undefined") {
+              lastTriggerTimePerformance = performance.now();
             }
-          },
-          onTransitionEnd: ({onTransitionEnd}) => (e: TransitionEvent) => {
-            if (!onTransitionEnd) { return; }
+            timeUpdateRequested = false;
+          }
+        },
+        onTransitionEnd: ({onTransitionEnd}) => (e: TransitionEvent) => {
+          if (!onTransitionEnd) { return; }
 
-            // Skip transitionEnd that comes <= 10ms after (reversing) a transition.
-            // In most cases this came from the previous transition.
-            let compareWith = lastTriggerTime;
-            if ((e.timeStamp as any) < 1000000000000 && lastTriggerTimePerformance) {
-              compareWith = lastTriggerTimePerformance;
-            }
-            if ((e.timeStamp as any) - compareWith <= 10) {
-              return;
-            }
+          // Skip transitionEnd that comes <= 10ms after (reversing) a transition.
+          // In most cases this came from the previous transition.
+          let compareWith = lastTriggerTime;
+          if ((e.timeStamp as any) < 1000000000000 && lastTriggerTimePerformance) {
+            compareWith = lastTriggerTimePerformance;
+          }
+          if ((e.timeStamp as any) - compareWith <= 10) {
+            return;
+          }
 
-            onTransitionEnd(e);
-          },
-        };
-      }),
-    onWillReceiveProps<PropsUnion>(
-      ({active}, {active: nextActive, requestTimeUpdate}) => {
-        if (active !== nextActive) {
-          requestTimeUpdate();
-        }
-      }),
-    onDidUpdate<PropsUnion>(
-      ({handleTimeUpdateRequest}) => handleTimeUpdateRequest(),
-    ),
-    integrate<keyof PropsUnion>(
-      "onTransitionEnd",
-    ),
+          onTransitionEnd(e);
+        },
+      };
+    }),
+  onWillReceiveProps<PropsUnion>(
+    ({active}, {active: nextActive, requestTimeUpdate}) => {
+      if (active !== nextActive) {
+        requestTimeUpdate();
+      }
+    }),
+  onDidUpdate<PropsUnion>(
+    ({handleTimeUpdateRequest}) => handleTimeUpdateRequest(),
+  ),
+  integrate<keyof PropsUnion>(
+    "onTransitionEnd",
   ),
 );
