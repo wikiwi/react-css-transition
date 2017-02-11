c76fdd2026edbfc0b565b0f73e24aa0c9a61171b
Chi Vinh Le <vinh@wikiwi.io>
2017-02-11 22:58:58 +0700

    Add font smoothing to docs

--- docs/src/global.css.ts
+++ docs/src/global.css.ts
@@ -1,5 +1,10 @@
 export default {
   body: {
     margin: "0",
+
+    /* Support for all WebKit browsers. */
+    WebkitFontSmoothing: "antialiased",
+    /* Support for Firefox. */
+    MozOsxFontSmoothing: "grayscale",
   },
 };
